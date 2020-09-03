ffe63854f76f4bcd95381e0107d6e01d9cf5834c
vagrant <sotahashimoto316@gmail.com>
2020-09-03 02:48:02 +0000

    memberAdd

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,23 @@
+class MembersController < ApplicationController
+  def show
+    @member = Member.find(params[:id])
+  end
+
+  def edit
+    @member = Member.find(params[:id])
+  end
+
+  def update
+    member = Member.find(params[:id])
+    if member.update(member_params)
+      redirect_to member_path(current_member)
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def member_params
+    params.require(:member).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :members
+
   root to: 'home#top'
   get 'about', to: 'home#about'
+
+  resources :members, only: [:show, :edit, :update]
 end
