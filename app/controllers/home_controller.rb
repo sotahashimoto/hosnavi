4a14446f5bb82ffb7ec53fe947bed22e00bc29ff
vagrant <sotahashimoto316@gmail.com>
2020-10-03 02:21:22 +0000

    guestloginAdd

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,13 @@ class HomeController < ApplicationController
 
   def about
   end
+
+  def new_guest
+    member = Member.find_or_create_by!(name: 'ゲスト太郎', nickname: 'ゲスト', email: 'guest@example.com') do |member|
+      member.password = SecureRandom.urlsafe_base64
+      # member.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+    sign_in member
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
 end
