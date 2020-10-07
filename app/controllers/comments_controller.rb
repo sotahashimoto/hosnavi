69adf5ebea3fc6249b171748e72b71e9ac72c7b5
vagrant <sotahashimoto316@gmail.com>
2020-10-07 04:16:43 +0000

    comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,8 @@ class CommentsController < ApplicationController
     @comment = current_member.comments.new(comment_params)
     @comment.hospital_id = @hospital.id
     if @comment.save
+      @comment = Comment.new
+      @comments = @hospital.comments
     else
       flash[:danger] = '空白で投稿はできません'
     end
