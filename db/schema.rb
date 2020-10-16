4142f84855415f1620bd443f1fc50b619131d24a
vagrant <sotahashimoto316@gmail.com>
2020-10-16 08:00:46 +0000

    reviewAdd

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_10_13_025325) do
   create_table "comments", force: :cascade do |t|
     t.integer "member_id"
     t.integer "hospital_id"
+    t.integer "score"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
