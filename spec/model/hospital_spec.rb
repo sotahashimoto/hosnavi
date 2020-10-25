ad9292e5fac96db82e5dcf90514356ef8909e5f6
vagrant <sotahashimoto316@gmail.com>
2020-10-25 05:46:17 +0000

    RspecFix

--- spec/model/hospital_spec.rb
+++ spec/model/hospital_spec.rb
@@ -4,11 +4,12 @@ RSpec.describe Hospital, type: :model do
   describe 'レビューのテスト' do
     context 'avg_scoreメソッド' do
       it "計算が正しいか" do
-        avg_score = create(:hospital, id: 1)
-        avg_score = build(:comment, id: 1, score: 1)
-        avg_score = build(:comment, id: 2, score: 1)
-        avg_score = build(:comment, id: 3, score: 1)
-        expect(hospital.self.avg_score).to eq 1
+        member = create(:member, name: "taro", email: "taro@example.com")
+        hospital = create(:hospital, name: "byouin")
+        avg_score1 = create(:comment, id: 1, member_id: 1, hospital_id: 1, score: 1)
+        avg_score2 = create(:comment, id: 2, member_id: 1, hospital_id: 1, score: 2)
+        avg_score3 = create(:comment, id: 3, member_id: 1, hospital_id: 1, score: 3)
+        expect(hospital.avg_score).to eq 2
       end
     end
   end
